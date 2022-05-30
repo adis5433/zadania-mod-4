a51280890c2d735c33d5d1d96a22f9222aac9b26
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-05-30 14:56:04 +0200

    wersja jedno linijkowa funkcji

--- palindrom.py
+++ palindrom.py
@@ -1,31 +1,17 @@
 
 
 def is_palindrome(word: str):
-    y = 0
-    x = -1
-    word_backward = []
-    for _ in word:
-        word_backward.append(word[x])
-        x -= 1
-    word_forward = []
-    for _ in word:
-        word_forward.append(word[y])
-        y += 1
-    if word_forward == word_backward:
-        return True
-    else:
-        return False
+    return word == (word[::-1])
 
 
 """
 returns boolean value,
 argument is str 
-znaki z argumentu są umieszczane w liście -word_forward pokolei
-oraz w liście - word_backward w odwrotnej kolejności
-następnie obie listy są porównywane zwracana jest wartość true jeśli są takie same
-a false jeśli różne
+
 """
 
 
 
-print(is_palindrome("tenet"))
+
+print(is_palindrome("potop"))
+
