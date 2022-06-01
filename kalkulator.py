a1310ed916550b1b2c4f50ccec561b2b0351d83b
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-06-01 08:23:28 +0200

    add input value verification,loggins and get_numbers function

--- kalkulator.py
+++ kalkulator.py
@@ -1,88 +1,92 @@
-
-def sum(*nums):
-    result = sum(nums)
-    return result
-
-def multipy(*nums):
-    result = 1
-    for num in nums:
-        result = result * num
-    return result
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 
-
-
-def reduction(first_num, second_num):
-    result = int(first_num) - int(second_num)
-    return result
-
-
-def divide(first_num, second_num):
-    result = int(first_num) / int(second_num)
-    return result
-
-
-print("Witaj w kalkulatorze\n"
-        "1 - dodawanie nieograniczonej ilości liczb\n"
-        "2 - mnożenie nieograniczonej ilości liczb\n"
-        "3 - odejmowanie dwóch licz\n"
-        "4 - dzielenie dwóch liczb\n"
-        "e - zamknij kalkulator\n")
-def calculator(user_choice):
-        match user_choice:
-            case "1":
-                print("wybrałeś dodawanie")
-                nums=[]
-                first_num = int(input("Podaj pierwszą liczbe : "))
-                nums.append(first_num)
-                second_num = int(input("Podaj drugą liczbę : "))
-                nums.append(second_num)
-                while (True):
-                    sum_choice = input("Chcesz podać kolejną liczbę(Y), czy przejść do wyniku(N)")
-                    if sum_choice.upper() =="Y":
-                        num = int(input("Podaj kolejną liczbę do dodania : "))
-                        nums.append(num)
-                        continue
-                    elif sum_choice.upper() =="N":
-                        print(sum(*nums))
-            case "2":
-                print("wybrałeś mnożenie")
+def get_numbers():
+        while (True):
+            try:
                 nums = []
                 first_num = int(input("Podaj pierwszą liczbe : "))
                 nums.append(first_num)
                 second_num = int(input("Podaj drugą liczbę : "))
                 nums.append(second_num)
-                while(True):
-                    sum_choice = input("Chcesz podać kolejną liczbę(Y), czy przejść do wyniku(N)")
-                    if sum_choice.upper() == "Y":
-                        num = int(input("Podaj kolejną liczbę do mnożenia : "))
-                        nums.append(num)
-                        continue
-                    elif sum_choice.upper() == "N":
-                        print(multipy(*nums))
+                logging.info ("///Liczby na których będzie wykonywane zadanie na ten moment to: %s" % nums)
+                sum_choice = input("Chcesz podać kolejną liczbę(Y), czy przejść do wyniku(N)")
+                if sum_choice.upper() == "Y":
+                        num = int(input("Podaj kolejną liczbę do dodania : "))
+                        nums.append(second_num)
+                        logging.info ("///Liczby na których będzie wykonywane zadanie na ten moment to: %s" % nums)
+                elif sum_choice.upper() == "N":
+                    logging.info ("///Ostatecznie będziemy wykonywali działanie na : %s" % nums)
+                    return nums
                     break
+            except:
+                logging.error ("///Podałeś nieprawidłową wartość")
+                ValueError
+
+
+
+def addition():
+    result = sum(get_numbers())
+    return "Wynik dodoawania to %s" % result
+
+def multipy():
+    result = 1
+    nums = get_numbers()
+    for num in nums:
+        result = result * num
+    return "Wynik mnożenia to %s" % result
+
+
+def reduction():
+    nums = get_numbers()
+    result=nums[0]*2
+    for num in nums:
+        result = result - num
+    return "Wynik odejmowania to %s" % result
+
+
+def divide():
+    nums = get_numbers()
+    result = nums[0]*nums[0]
+    for num in nums:
+        result = result / num
+    return "Wynik dzielenia to %s" % result
+
+
+def calculator(user_choice):
+        match user_choice:
+            case "1":
+                print("Wybrałeś dodawanie")
+                print(addition())
+            case "2":
+                print("Wybrałeś mnożenie")
+                print(multipy())
             case "3":
-                print("wybrałeś odejmowanie")
-                first_num = int(input("Podaj pierwszą liczbę : "))
-                second_num = int(input("Podaj pierwszą liczbę : "))
-                print(reduction(first_num, second_num))
+                print("Wybrałeś odejmowanie")
+                print(reduction())
             case "4":
-                print("wybrałeś dzielenie")
-                first_num = int(input("Podaj pierwszą liczbę : "))
-                second_num = int(input("Podaj pierwszą liczbę : "))
-                print(divide(first_num, second_num))
+                print("Wybrałeś dzielenie")
+                try:
+                    print(divide())
+                except:
+                    ZeroDivisionError
+                    logging.error("///nie dzielimy przez zero")
             case "e" :
+                print("Kończymy program")
                 exit()
             case _:
+                logging.error("///Podałeś wartość z poza menu podaj jeszcze raz prawidłową")
                 print(f"Nie prawidłowa wartość!")
 
 
 
-
-
-
-
-
-user_choice=input("Którą z opcji wybierasz...")
-
-calculator(user_choice)
+while(True):
+        print("Witaj w kalkulatorze\n"
+            "1 - dodawanie nieograniczonej ilości liczb\n"
+            "2 - mnożenie nieograniczonej ilości liczb\n"
+            "3 - odejmowanie dwóch licz\n"
+            "4 - dzielenie dwóch liczb\n"
+            "e - zamknij kalkulator\n")
+        user_choice=input("Którą z opcji wybierasz...")
+        calculator(user_choice)
